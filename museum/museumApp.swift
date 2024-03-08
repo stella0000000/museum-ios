db1b9b23947986204e3dfc1f7a7daccc3a0e53ed
stella <stella.choi@hyperisland.se>
2024-03-07 23:36:11 -0500

    Initial Commit

---
+++ museum/museumApp.swift
@@ -0,0 +1,17 @@
+//
+//  museumApp.swift
+//  museum
+//
+//  Created by stella choi on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct museumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
